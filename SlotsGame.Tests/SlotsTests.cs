2421db88c1eaa29ebb028b964752e147aeb4cf50
Robert Lyn <43123823+RobertLynJA@users.noreply.github.com>
2025-02-14 00:07:52 -0500

    Add Slots

---
+++ SlotsGame.Tests/SlotsTests.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Xml.Linq;
+
+namespace SlotsGame.Tests;
+
+public class SlotsTests
+{
+    [Fact] 
+    public void Constructor_Valid_CreateSlots() 
+    {
+        // Arrange
+        int reels = 3;
+        Symbol[] symbols = { new Symbol("1", 1), new Symbol("2", 2), new Symbol("3", 3) };
+        
+        // Act
+        var slots = new Slots(reels, symbols);
+
+        // Assert
+        Assert.Equal(3, slots.Symbols.Count());
+    }
+
+    [Fact]
+    public void Constructor_EmptySymbols_ThrowsException()
+    {
+        // Arrange
+        int reels = 3;
+        Symbol[] symbols = { };
+
+        // Act
+        var exception = Assert.Throws<ArgumentException>(
+            () => new Slots(reels, symbols)
+        );
+
+        // Assert
+        Assert.Equal("Symbols cannot be empty", exception.Message);
+    }
+
+    [Fact]
+    public void Constructor_NegativeReels_ThrowsException()
+    {
+        // Arrange
+        int reels = -1;
+        Symbol[] symbols = { new Symbol("1", 1), new Symbol("2", 2), new Symbol("3", 3) };
+
+        // Act
+        var exception = Assert.Throws<ArgumentException>(
+            () => new Slots(reels, symbols)
+        );
+
+        // Assert
+        Assert.Equal("Reels must be greater than 0", exception.Message);
+    }
+
+    [Fact]
+    public void Constructor_ZeroReels_ThrowsException()
+    {
+        // Arrange
+        int reels = 0;
+        Symbol[] symbols = { new Symbol("1", 1), new Symbol("2", 2), new Symbol("3", 3) };
+
+        // Act
+        var exception = Assert.Throws<ArgumentException>(
+            () => new Slots(reels, symbols)
+        );
+
+        // Assert
+        Assert.Equal("Reels must be greater than 0", exception.Message);
+    }
+}
---
+++ SlotsGame/Slots.cs
@@ -0,0 +1,23 @@
+﻿namespace SlotsGame;
+
+public class Slots
+{
+    public int Reels { get; }
+    public IEnumerable<Symbol> Symbols { get; }
+
+    public Slots(int reels, params Symbol[] symbols) 
+    {
+        if (reels < 1) 
+        {
+            throw new ArgumentException("Reels must be greater than 0");
+        }
+
+        if (symbols.Length < 1)
+        {
+            throw new ArgumentException("Symbols cannot be empty");
+        }
+
+        Reels = reels;
+        Symbols = symbols;
+    }
+}
