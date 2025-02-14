529ba875cf1d0663d8be1d584d2e69223a74756e
Robert Lyn <43123823+RobertLynJA@users.noreply.github.com>
2025-02-13 20:19:25 -0500

    Add Symbol

---
+++ SlotsGame.Tests/SymbolTests.cs
@@ -0,0 +1,33 @@
+﻿namespace SlotsGame.Tests;
+
+public class SymbolTests
+{
+    [Fact]
+    public void Constructor_Valid_CreatesSymbol()
+    {
+        var symbol = new Symbol("1", 1);
+
+        Assert.Equal("1", symbol.Name);
+        Assert.Equal(1, symbol.Value);
+    }
+
+    [Fact]
+    public void Constructor_ZeroValue_ThrowsException()
+    {
+        var exception = Assert.Throws<ArgumentException>(
+            () => new Symbol("1", 0)
+        );
+        
+        Assert.Equal("Value must be greater than 0", exception.Message);
+    }
+
+    [Fact]
+    public void Constructor_NegativeValue_ThrowsException()
+    {
+        var exception = Assert.Throws<ArgumentException>(
+            () => new Symbol("1", -1)
+        );
+
+        Assert.Equal("Value must be greater than 0", exception.Message);
+    }
+}
---
+++ SlotsGame/Symbol.cs
@@ -0,0 +1,18 @@
+﻿namespace SlotsGame;
+
+public class Symbol
+{
+    public string Name { get; }
+    public int Value { get; }
+
+    public Symbol(string name, int value)
+    {
+        if (value < 1) 
+        {
+            throw new ArgumentException("Value must be greater than 0");
+        }
+
+        Name = name;
+        Value = value;
+    }
+}
